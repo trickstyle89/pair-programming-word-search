54dfde70140d44c8133455575a00e00bbc694697
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-15 22:27:37 -0500

    weds

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,56 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEST');
+  
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'DAVY');
+    
     assert.isTrue(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,59 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  // Join the rows of the letters array into an array of strings
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const reverseWord = word.split('').reverse().join('');
+  console.log(reverseWord);
+  // Check if the word appears in any of the rows across.
+  // very simple and limited
+  for (let row of horizontalJoin) {
+    if (row.includes(word) || row.includes(reverseWord)) {
+      return true;
     }
-}
+  }
+  
+  // Check if the word appears in any of the columns
+  for (let i = 0; i < letters.length; i++) {
+    let column = '';
+    for (let j = 0; j < letters[0].length; j++) {
+      column += letters[j][i];
+    }
+    if (column.includes(word) || column.includes(reverseWord)) {
+      return true;
+    }
+  }
+  
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[0].length; j++) {
+      // Check if the word appears in the diagonal that starts at this character
+      let diagonal = '';
+      for (let k = 0; k < word.length; k++) {
+        if (i + k < letters.length && j + k < letters[0].length) {
+          diagonal += letters[i + k][j + k];
+        }
+      }
+      if (diagonal.includes(word) || diagonal.includes(reverseWord)) {
+        return true;
+      }
+    }
+  }
 
-module.exports = wordSearch
+  // Search for diagonals that slope downwards from left to right
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[0].length; j++) {
+      // Check if the word appears in the diagonal that starts at this character
+      let diagonal = '';
+      for (let k = 0; k < word.length; k++) {
+        if (i - k >= 0 && j + k < letters[0].length) {
+          diagonal += letters[i - k][j + k];
+        }
+      }
+      if (diagonal.includes(word) || diagonal.includes(reverseWord))
+        return true;
+    }
+  }
+  return false;
+};
+  
+// If the word doesn't appear in any rows or columns, return false
+
+module.exports = wordSearch;
